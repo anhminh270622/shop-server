7ff47d00710ac6a3b92d22c60022b09defa847bb
anhminh270622 <20020444@vnu.edu.vn>
2023-05-16 20:43:26 +0700

    3

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+const cors = require('cors');
+server.use(cors());
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
@@ -17,3 +19,4 @@ server.listen(3000, () => {
 
 // Export the Server API
 module.exports = server
+
